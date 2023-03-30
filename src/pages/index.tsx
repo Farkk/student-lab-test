b0ca62c11b0abf499a01a476b3cccf451972cf06
Pavel <blazem.show@mail.ru>
2023-03-30 13:35:57 +0600

    6.0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { DataQuotes } from '@/components/models/models'
 import MainLayout from '@/components/mainLayout/MainLayout'
 import Quote from '@/components/quote/Quote'
-	
+import { requestPopularQuotes } from '@/utilities/requests/requestPopularQuotes'
+import { requestAllTags } from '@/utilities/requests/requestAllTags'
+
 export default function Home({ data, tags }: DataQuotes) {
 	return (
 		<MainLayout header={'Popular quotes'} tags={tags}>
--- src/pages/tag/[id].tsx
+++ src/pages/tag/[id].tsx
@@ -2,7 +2,8 @@ import { DataQuotesTag, Query } from '@/components/models/models'
 import MainLayout from '@/components/mainLayout/MainLayout'
 import Quote from '@/components/quote/Quote'
 import { firstSymbolToUpperCase } from '@/utilities/textToUpperCase/firstSymbolToUpperCase'
-
+import { requestByTag } from '@/utilities/requests/requestByTag'
+import { requestAllTags } from '@/utilities/requests/requestAllTags'
 
 export default function Tag({ data, tags, query }: DataQuotesTag) {
 	const newStr = firstSymbolToUpperCase(query)
@@ -22,7 +23,7 @@ export default function Tag({ data, tags, query }: DataQuotesTag) {
 Tag.getInitialProps = async ({ query }: Query) => {
 	const jsonData = await requestByTag(query.id)
 	const jsonTags = await requestAllTags()
-	
+
 	return {
 		data: jsonData,
 		tags: jsonTags,
